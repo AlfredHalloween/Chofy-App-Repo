aa3453382e7c9b9b5160e693b6453c43b542d041
Juan Alfredo García González <sensei_12c4@hotmail.com>
2021-06-15 11:09:13 -0500

    remove loader indicator

--- libraries/ChofyExtensions/ChofyExtensions/Classes/UIViewController/UIViewControllerExtension.swift
+++ libraries/ChofyExtensions/ChofyExtensions/Classes/UIViewController/UIViewControllerExtension.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
+public var appWindow: UIWindow? { return UIApplication.shared.delegate?.window ?? nil }
+
+public var windowRootViewController: UIViewController? { return appWindow?.rootViewController }
+
+public var windowTopViewController: UIViewController? {
+    func finder(from viewController: UIViewController?) -> UIViewController? {
+        if let tabBarViewController = viewController as? UITabBarController {
+            return finder(from: tabBarViewController.selectedViewController)
+        } else if let navigationController = viewController as? UINavigationController {
+            return finder(from: navigationController.visibleViewController)
+        } else if let presenterViewController = viewController?.presentedViewController {
+            return finder(from: presenterViewController)
+        } else {
+            return viewController
+        }
+    }
+    return finder(from: windowRootViewController)
+}
+
 public extension UIViewController {
     
     func setViewBackground(color: UIColor) {
